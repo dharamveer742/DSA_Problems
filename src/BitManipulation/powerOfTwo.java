892a54126854dc81c5daaf77a35ac969e083b51e
dharamveer <dharamveer.gla_cs20@gla.ac.in>
2023-04-27 06:08:02 +0530

    optimised solution

--- src/BitManipulation/powerOfTwo.java
+++ src/BitManipulation/powerOfTwo.java
@@ -1,13 +1,27 @@
 package BitManipulation;
 
 public class powerOfTwo {
+
+    // Naive Approach O(log2N)
+    public static boolean isTwosPower(int no){
+        if(no==0) return  false;
+        while(no>1){
+            if(no%2!=0){
+                return  false;
+            }
+            no=no/2;
+        }
+        return true;
+    }
+ // optimised Approach O(1)
     public static boolean isPowerOfTwo(int no){
         if(no==0) return false;
         return (no&no-1)==0;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPowerOfTwo(10));
+
+        System.out.println(isTwosPower(16));
     }
 
 
--- src/BitManipulation/powxandn.java
+++ src/BitManipulation/powxandn.java
@@ -2,31 +2,26 @@ package BitManipulation;
 
 public class powxandn {
     // given two integers x and n . we have to find x^n
-    // -2^31<=n<=2^31-1   -2147483648  || if n is negative , and we convert into positive value it may give integer overflow  so use long or double datatype
+    // -2^31<=n<=2^31-1   -2147483648  || if n is negative , and we convert into positive value it may give integer overflow  so use long datatype
     //  brute force approach :- multiply x n times TC = O(N)
 
     // optimised TC = O(log2N)
 
     public static double pow(double x,int n){
-        double newN = n;
-        int flag =0;
-        if(n<0){
-            newN = -n ;
-            flag=1;
+        long newN = n;
+        if(newN<0){
+            newN = -newN ;
+            x=1/x;
         }
         double ans=1;
-        while(newN!=0){
-            if(newN%2==0){   //  2*7
-                x*=x;
-                newN=newN/2;
+        while(newN!=0) {
+            if (newN % 2 == 0) {
+                x *= x;
+                newN = newN / 2;
+            } else {
+                ans *= x;
+                newN = newN - 1;
             }
-            else{
-                ans*=x;
-                newN=newN-1;
-            }
-        }
-        if(flag==1){
-            return 1/ans;
         }
         return ans;
     }
