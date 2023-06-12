3340ee2e2485ba97582567ffd069b513707a9719
dharamveer <dharamveer.gla_cs20@gla.ac.in>
2023-06-12 23:46:56 +0530

    stock span problem

--- src/Stack/stockSpan.java
+++ src/Stack/stockSpan.java
@@ -1,2 +1,51 @@
-package Stack;public class stockSpan {
+package Stack;
+
+import javafx.util.Pair;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class stockSpan {
+    public static int[] stockSpan(int[] stock){
+        Stack<Pair<Integer,Integer>> leftGreater = new Stack<>();
+
+        int[] span = new int[stock.length];
+        int index=0;
+        for(int i=0;i<stock.length;i++){
+            if(leftGreater.isEmpty()){
+                span[index]=i-(-1);
+                index++;
+            }
+            else if(!leftGreater.isEmpty() && leftGreater.peek().getKey()>stock[i]){
+                span[index]=i-leftGreater.peek().getValue();
+                index++;
+            }
+            else {
+                while(!leftGreater.isEmpty() && leftGreater.peek().getKey()<=stock[i]){
+                    leftGreater.pop();
+                }
+                if(!leftGreater.isEmpty() && leftGreater.peek().getKey()>stock[i]){
+                    span[index]=i-leftGreater.peek().getValue();
+                    index++;
+                }
+                else{
+                    span[index]=i-1;
+                    index++;
+                }
+            }
+            leftGreater.push(new Pair(stock[i],i));
+        }
+        return span;
+    }
+
+    public static void main(String[] args) {
+        int[] stocks = {100,80,60,70,60,75,85};
+        int[] ans = stockSpan(stocks);
+        System.out.println(Arrays.toString(ans));
+    }
 }
+// 100 80 60 70 60 75 85
+// 1 1 1 2 1 4 6
+
+
+
