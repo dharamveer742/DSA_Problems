84e4eac164de0ae26f0b23b579e4f9d5fcd8e7c2
dharamveer <dharamveer.gla_cs20@gla.ac.in>
2023-07-27 13:23:18 +0530

    recursively reversed the linkedList

--- src/linkedList/reverse.java
+++ src/linkedList/reverse.java
@@ -2,42 +2,30 @@ package linkedList;
 
 public class reverse {
 
-    public static node reverse(node head){
-        return reverseRecursively(head,head);
-    }
-    public static node reverseRecursively(node head,node headRef){
-        if(head==null){
-            return  null;
-        }
-        node first = head;
-        node rest = first.next;
-        if(rest==null){
-            return first;
-        }
-        headRef=reverseRecursively(rest,rest);
-        rest.next=first;
-        first.next=null;
 
-        return headRef;
+    public static Node reverseRecursively(Node head){
+        if(head==null || head.next==null){
+            return head;
+        }
+        Node newHead= reverseRecursively(head.next);
+        head.next.next=head;
+        head.next=null;
+        return newHead;
+
     }
+
+
 
     public static void main(String[] args) {
-        node n1 = new node(10);
-        node n2 = new node(20);
-        node n3 =  new node(30);
-        node n4 = new node(40);
+        Node n1 = new Node(10);
+        Node n2 = new Node(20);
+        Node n3 =  new Node(30);
+        Node n4 = new Node(40);
         n1.next=n2;
         n2.next=n3;
         n3.next=n4;
-        System.out.println(reverse(n1).next.data);
+        System.out.println(reverseRecursively(n1).next.next.data);
 
     }
 
-}
-class node{
-    int data;
-    node next;
-    node(int d){
-        data=d;
-    }
 }
